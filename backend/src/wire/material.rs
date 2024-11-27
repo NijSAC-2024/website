e66f482518f9733a4ee18da8de944a5af2d6f5ad
robintup598 <rylput@gmail.com>
2024-11-27 07:35:28 -0600

    Added to (User)Material

---
+++ backend/src/wire/material.rs
@@ -0,0 +1,28 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+use validator::Validate;
+
+#[derive(Debug, Serialize, Deserialize, Validate)]
+pub struct Material {
+    pub material_id: Uuid,
+    #[validate(length(min = 1, max = 100))]
+    pub name_eng: String,
+    #[validate(length(min = 1, max = 100))]
+    pub name_nl: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Validate)]
+pub struct UserMaterial {
+    pub user_id: Uuid,
+    pub material_id: Uuid,
+    #[validate(range(
+        min = 0,
+        max = 999_999_999,
+        message = "material amount has a maximum of 9 numbers"
+    ))]
+    pub material_amount: i32,
+}
+
+
+
+
