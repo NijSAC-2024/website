819df5ad57c996f28f475d01304aa11599d418af
Maximilian Pohl <maximilian@tweedegolf.com>
2025-02-02 16:16:40 +0100

    WIP

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM node:22 AS frontend
+
+WORKDIR /src
+COPY ./frontend/package.json ./frontend/package-lock.json ./
+
+RUN npm install
+
+COPY ./frontend ./
+
+RUN npm run build
+
+FROM rust:latest AS backend
+
+WORKDIR /src
+COPY ./backend/ ./
+COPY --from=frontend /src/dist/ /frontend/dist/
+
+RUN cargo build --release
+
+FROM debian:bookworm-slim AS final
+RUN apt-get update && apt-get install curl -y
+
+ARG user=nonroot
+ARG group=nonroot
+ARG uid=2000
+ARG gid=2000
+RUN addgroup --gid ${gid} ${group} && adduser --uid ${uid} --gid ${gid} --system --disabled-login --disabled-password ${user}
+EXPOSE 3000
+
+COPY --from=backend /src/target/release/nijsac-website-backend /home/nonroot/nijsac-website-backend
+
+RUN chmod 777 /home/nonroot/nijsac-website-backend
+
+USER $user
+
+ENTRYPOINT ["./home/nonroot/nijsac-website-backend"]
--- frontend/src/pages/TextPage.tsx
+++ frontend/src/pages/TextPage.tsx
@@ -1,21 +1,22 @@
 import GenericPage from './GenericPage.tsx';
-import ContentCard from '../components/ContentCard.tsx';
-import MarkdownEditor from '../components/markdown/MarkdownEditor.tsx';
-import { AgendaEventType, LanguageType } from '../types.ts';
-import { Moment } from 'moment';
+// import ContentCard from '../components/ContentCard.tsx';
+// import MarkdownEditor from '../components/markdown/MarkdownEditor.tsx';
+// import { AgendaEventType, LanguageType } from '../types.ts';
+// import { Moment } from 'moment';
 
 export default function TextPage() {
-  const handleFieldChange = (
-    name: keyof AgendaEventType,
-    value: string | Moment | boolean | LanguageType
-  ) => {
-    console.log(name, value);
-  };
+  // const handleFieldChange = (
+  //   name: keyof AgendaEventType,
+  //   value: string | Moment | boolean | LanguageType
+  // ) => {
+  //   console.log(name, value);
+  // };
   return (
     <GenericPage>
-      <ContentCard className="p-7">
-        <MarkdownEditor handleFieldChange={handleFieldChange} />
-      </ContentCard>
+      <h1>TODO</h1>
+      {/*<ContentCard className="p-7">*/}
+        {/*<MarkdownEditor handleFieldChange={handleFieldChange} />*/}
+      {/*</ContentCard>*/}
     </GenericPage>
   );
 }
